2b90e562a28113e81c8259a2a0e2b851bf5bce2e
Jonathan Plasse <jonathan.plasse@live.fr>
2024-06-17 18:42:14 +0200

    Generalize profile calculation

--- curve_paradise/__init__.py
+++ curve_paradise/__init__.py
@@ -1,54 +1,37 @@
 import numpy as np
+from math import factorial
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Slider
 from bokeh.plotting import figure
 
 
-def cubic(x, a, b, c, d):
-    return a / 6 * x**3 + b / 2 * x**2 + c * x + d
+def interpolate(x, coeffs, deg):
+    return sum(
+        c * x**i / factorial(i) for i, c in zip(reversed(range(deg + 1)), coeffs, strict=False)
+    )
 
 
-def quadratic(x, a, b, c):
-    return a / 2 * x**2 + b * x + c
-
-
-def linear(x, a, b):
-    return a * x + b
-
-
-def calculate_profile(t, j):
+def calculate_profile(t, ts, js, init=(0, 0, 0)):
     a = np.zeros_like(t)
     v = np.zeros_like(t)
     p = np.zeros_like(t)
 
-    t_1_4 = 1 / 4
-    t_3_4 = 3 / 4
+    ts = [0, 1 / 4, 3 / 4]
 
-    a_1_4 = linear(t_1_4, j, 0)
-    a_3_4 = linear(t_3_4 - t_1_4, -j, a_1_4)
+    a_0, v_0, p_0 = init
 
-    v_1_4 = quadratic(t_1_4, j, 0, 0)
-    v_3_4 = quadratic(t_3_4 - t_1_4, -j, a_1_4, v_1_4)
-
-    p_1_4 = cubic(t_1_4, j, 0, 0, 0)
-    p_3_4 = cubic(t_3_4 - t_1_4, -j, a_1_4, v_1_4, p_1_4)
-
-    mask_0 = t < t_1_4
-    mask_1_4 = (t >= t_1_4) & (t < t_3_4)
-    mask_3_4 = t >= t_3_4
-
-    a[mask_0] = linear(t[mask_0], j, 0)
-    a[mask_1_4] = linear(t[mask_1_4] - t_1_4, -j, a_1_4)
-    a[mask_3_4] = linear(t[mask_3_4] - t_3_4, j, a_3_4)
-
-    v[mask_0] = quadratic(t[mask_0], j, 0, 0)
-    v[mask_1_4] = quadratic(t[mask_1_4] - t_1_4, -j, a_1_4, v_1_4)
-    v[mask_3_4] = quadratic(t[mask_3_4] - t_3_4, j, a_3_4, v_3_4)
-
-    p[mask_0] = cubic(t[mask_0], j, 0, 0, 0)
-    p[mask_1_4] = cubic(t[mask_1_4] - t_1_4, -j, a_1_4, v_1_4, p_1_4)
-    p[mask_3_4] = cubic(t[mask_3_4] - t_3_4, j, a_3_4, v_3_4, p_3_4)
+    for i, ji in enumerate(js):
+        is_last = i == len(js) - 1
+        mask = (t >= ts[i]) & (t < ts[i + 1]) if not is_last else t >= ts[i]
+        poly = [ji, a_0, v_0, p_0]
+        a[mask] = interpolate(t[mask] - ts[i], poly, deg=1)
+        v[mask] = interpolate(t[mask] - ts[i], poly, deg=2)
+        p[mask] = interpolate(t[mask] - ts[i], poly, deg=3)
+        if not is_last:
+            a_0 = interpolate(ts[i + 1] - ts[i], poly, deg=1)
+            v_0 = interpolate(ts[i + 1] - ts[i], poly, deg=2)
+            p_0 = interpolate(ts[i + 1] - ts[i], poly, deg=3)
 
     return a, v, p
 
@@ -67,7 +50,11 @@ slider_d = Slider(start=-5, end=5, value=d_init, step=0.1, title="d_max")
 
 
 def get_data():
-    a, v, p = calculate_profile(time, slider_a.value)
+    a, v, p = calculate_profile(
+        time,
+        [0, 1 / 4, 3 / 4],
+        [slider_a.value, -slider_a.value, slider_a.value],
+    )
     return {"time": time, "position": p, "velocity": v, "acceleration": a}
 
 
